dacd133dfdd9a6857a286d405567de3faec0d8f2
ZAKARIA TOURAS <zakariatouras47@gmail.com>
2024-05-19 18:54:28 -0700

    cityClass

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''city class'''
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    '''
+    define city class
+    '''
+    state_id = ''
+    name = ''
