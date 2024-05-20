114113acb95e08516e86969126e1a6e1a4185c6d
ZAKARIA TOURAS <zakariatouras47@gmail.com>
2024-05-19 18:58:35 -0700

    UserClass

---
+++ models/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+'''User class'''
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    '''
+    define User class
+    '''
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
